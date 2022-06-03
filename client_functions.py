56ebdc58dc7f23530930e8f6d00202d43a737eba
avidalto <avidalto@hawk.iit.edu>
2022-06-03 16:34:07 -0500

    Added dated print

--- client_functions.py
+++ client_functions.py
@@ -1,52 +1,57 @@
 from client import Client
 import sys, json
 from time import sleep
+from datetime import datetime
+
+def printd(*args):
+    print(datetime.now(), *args)
+
 
 def start_resource(resource_name, c):
-    print('Starting resource {}'.format(resource_name))
+    printd('Starting resource {}'.format(resource_name))
     # check if resource exists and is running already
     resource = c.get_resource(resource_name)
     if resource:
         if resource['status'] == "off":
             c.start_resource(resource_name)
-            print('{} started'.format(resource_name))
+            printd('{} started'.format(resource_name))
             return 'started'
         else:
-            print('{} already running'.format(resource_name))
+            printd('{} already running'.format(resource_name))
             return 'already-running'
     else:
-        print('{} not found'.format(resource_name))
+        printd('{} not found'.format(resource_name))
         return 'not-found'
 
 def stop_resource(resource_name, c):
-    print('Stopping resource {}'.format(resource_name))
+    printd('Stopping resource {}'.format(resource_name))
     # check if resource exists and is stopped already
     resource = c.get_resource(resource_name)
     if resource:
         if resource['status'] == "off":
-            print('{} already stopped'.format(resource_name))
+            printd('{} already stopped'.format(resource_name))
             return 'already-stopped'
         else:
             c.stop_resource(resource_name)
-            print('{} stopped'.format(resource_name))
+            printd('{} stopped'.format(resource_name))
             return 'stopped'
     else:
-        print('{} not found'.format(resource_name))
+        printd('{} not found'.format(resource_name))
         return 'not-found'
     
     
 def launch_workflow(wf_name, wf_xml_args, user, c):
-    print('Launching workflow {wf} in user {user}'.format(
+    printd('Launching workflow {wf} in user {user}'.format(
         wf = wf_name,
         user = user
     ))   
-    print('XML ARGS: ', json.dumps(wf_xml_args, indent = 4))
+    printd('XML ARGS: ', json.dumps(wf_xml_args, indent = 4))
     jid,djid = c.start_job(wf_name, wf_xml_args, user)
     return jid, djid
 
 
 def wait_workflow(djid, wf_name, c):
-    print('Waiting for workflow', wf_name)
+    printd('Waiting for workflow', wf_name)
     while True:
         try:
             state = c.get_job_state(djid)
@@ -58,7 +63,7 @@ def wait_workflow(djid, wf_name, c):
         elif (state == 'deleted' or state == 'error'):
             raise Exception('Simulation had an error. Please try again')
 
-        print('Workflow', wf_name, 'state:', state)
+        printd('Workflow', wf_name, 'state:', state)
         sleep(10)
 
-    print(wf_name, 'completed successfully')
+    printd(wf_name, 'completed successfully')
--- multicluster_parsl_demo.sh
+++
@@ -1,7 +0,0 @@
-python run_workflow.py \
-       canary-user-01.parallel.works \
-       44129863fe41d05e862fd31ef6f709b8 \
-       alvarovidalto \
-       atnorth---gcpslurmv2 \
-       multicluster_parsl_demo
-       
